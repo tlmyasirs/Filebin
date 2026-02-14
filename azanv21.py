1b228e5bf8ea6c4437bd61dccc5ea96fb06fcca5
Yasir TLM <tlmyasirs@yahoo.com>
2026-02-14 07:44:53 +0530

    Fix Isha prayer time from 25 to 30 minutes

--- azanv21.py
+++ azanv21.py
@@ -183,7 +183,7 @@ def setup_main_ui(bg_photo, bg_photo1):
     label_minutes = {
         "Subahu": 30,
         "Sunrise": 20,
-        "Isha: 25,
+        "Isha": 30,
         "Maghrib": 10,
     }
 
